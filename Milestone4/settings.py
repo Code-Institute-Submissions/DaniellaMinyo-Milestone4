8449f91d658fcd5eb3e7b267b8672ae38cbb3102
DaniellaMinyo <daniellaminyo99@gmail.com>
2021-11-06 09:59:28 +0000

    deployment part 2

--- Milestone4/settings.py
+++ Milestone4/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-mgc0u67$ug%!c%8ejflxkx3yywjbu2*0a3lm!p6=p#ggv_o1%0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milestone4-project-daniella-m.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'Milestone4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
