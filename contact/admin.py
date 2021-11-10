18986a16d9bc61eddb003d3778bb1a5a1b98317c
DaniellaMinyo <daniellaminyo99@gmail.com>
2021-11-10 06:17:33 +0000

    add forms and admin to contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    fields = ('full_name', 'email', 'subject', 'message')
+    list_display = ('full_name', 'email', 'subject', 'message')
+
+
+admin.site.register(Contact, ContactAdmin)
--- contact/models.py
+++ contact/models.py
@@ -7,7 +7,7 @@ class Contact(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     subject = models.CharField(max_length=200, null=False, blank=False)
-    full_name = models.CharField(max_length=5000, null=False, blank=False)
+    message = models.CharField(max_length=5000, null=False, blank=False)
 
     def __str__(self):
         return self.full_name
