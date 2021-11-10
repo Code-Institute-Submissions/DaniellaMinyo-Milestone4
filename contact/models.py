db889d95ea2ca30acaa10995ebe6c7152356ffb3
DaniellaMinyo <daniellaminyo99@gmail.com>
2021-11-10 17:36:45 +0000

    change contact models

--- contact/models.py
+++ contact/models.py
@@ -7,7 +7,8 @@ class Contact(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     subject = models.CharField(max_length=200, null=False, blank=False)
-    message = models.CharField(max_length=5000, null=False, blank=False)
+    message = models.CharField(
+        max_length=5000, null=False, blank=False, default='SOME STRING')
 
     def __str__(self):
         return self.full_name
