fd098af64f93266313499ca5c6268a08564fc695
DaniellaMinyo <daniellaminyo99@gmail.com>
2021-11-12 17:27:01 +0000

    change default contact message value to an empty string

--- contact/models.py
+++ contact/models.py
@@ -8,7 +8,7 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, null=False, blank=False)
     subject = models.CharField(max_length=200, null=False, blank=False)
     message = models.CharField(
-        max_length=5000, null=False, blank=False, default='SOME STRING')
+        max_length=5000, null=False, blank=False, default='')
 
     def __str__(self):
         return self.full_name
